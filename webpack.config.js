a7f26b78ffac19b32872e72f3a830005e06cbe6e
Shmitkin <mitkinoleg@gmail.com>
2020-06-11 17:27:20 +0300

    Настроил сборку проекта

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+const path = require(`path`);
+const pathToPublic = path.join(__dirname, `public`);
+
+module.exports = {
+  entry: `./src/index.js`,
+  output: {
+    filename: `bundle.js`,
+    path: pathToPublic
+  },
+  devServer: {
+    contentBase: pathToPublic,
+    open: true,
+    port: 1337,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: `babel-loader`,
+        },
+      }
+    ],
+  },
+  devtool: `source-map`,
+};
