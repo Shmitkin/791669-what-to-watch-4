97adc55fb24bfa92b82fc88b4326bc151fe85fc4
Shmitkin <mitkinoleg@gmail.com>
2020-07-05 22:53:31 +0300

    Рефакторинг, выносит логику валидации и состояний в хок

--- src/components/sign-in-page/sign-in-page.jsx
+++ src/components/sign-in-page/sign-in-page.jsx
@@ -2,18 +2,35 @@ import React from "react";
 import PageHeader from "../page-header/page-header.jsx";
 import PageFooter from "../page-footer/page-footer.jsx";
 import SignInForm from "../sign-in-form/sign-in-form.jsx";
+import {Redirect} from "react-router-dom";
+import {connect} from "react-redux";
+import withFormValidation from "../../hocs/with-form-validation.jsx";
 
-export default function SignInPage() {
+const WrappedSignInForm = withFormValidation(SignInForm);
 
-  return (
-    <div className="user-page">
-      <PageHeader extraClass={`user-page__head`}>
-        <h1 className="page-title user-page__title">Sign in</h1>
-      </PageHeader>
-      <div className="sign-in user-page__content">
-        <SignInForm />
+export function SignInPage({isAuth}) {
+
+  if (isAuth) {
+    return <Redirect from="/login" to="/" />;
+  } else {
+    return (
+      <div className="user-page">
+        <PageHeader extraClass={`user-page__head`}>
+          <h1 className="page-title user-page__title">Sign in</h1>
+        </PageHeader>
+        <div className="sign-in user-page__content">
+          <WrappedSignInForm />
+        </div>
+        <PageFooter />
       </div>
-      <PageFooter />
-    </div>
-  );
+    );
+  }
 }
+
+const mapStateToProps = (state) => ({
+  isAuth: state.isUserAuth
+});
+
+export default connect(mapStateToProps)(SignInPage);
+
+
---
+++ src/hocs/with-form-validation.jsx
@@ -0,0 +1,131 @@
+import React, {PureComponent} from "react";
+import PropTypes from "prop-types";
+import {connect} from "react-redux";
+import {ActionCreator} from "../reducer/reducer";
+
+export default function withFormValidation(Component) {
+  class WithFormValidation extends PureComponent {
+    constructor(props) {
+      super(props);
+
+      this.state = {
+        errorMessage: ``,
+        emailValidity: true,
+        passValidity: true,
+      };
+
+      this._email = null;
+      this._pass = null;
+
+      this._onFormSubmit = this._onFormSubmit.bind(this);
+      this._onFormChange = this._onFormChange.bind(this);
+      this._onInputFocus = this._onInputFocus.bind(this);
+    }
+
+    _isEmailCorrect() {
+      const emailPattern = new RegExp(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/);
+      return (emailPattern.test(this._email));
+    }
+
+    _isPasswordTyped() {
+      if (this._pass) {
+        return true;
+      } else {
+        return false;
+      }
+    }
+
+    _checkValidity(emailValidity, passValidity) {
+      if (!emailValidity && !passValidity) {
+        this.setState({
+          errorMessage: `Please enter a valid email address and your password`,
+          emailValidity: false,
+          passValidity: false,
+        });
+      } else if (!emailValidity) {
+        this.setState({
+          errorMessage: `Please enter a valid email address`,
+          emailValidity: false,
+          passValidity: true,
+        });
+      } else if (!passValidity) {
+        this.setState({
+          errorMessage: `Please enter your password`,
+          emailValidity: true,
+          passValidity: false,
+        });
+      } else {
+        this.setState({
+          errorMessage: ``,
+          emailValidity: true,
+          passValidity: true,
+        });
+      }
+    }
+
+    _onFormChange(target) {
+      switch (target.type) {
+        case `email`:
+          this._email = target.value;
+          break;
+        case `password`:
+          this._pass = target.value;
+          break;
+      }
+    }
+
+    _onInputFocus(target) {
+      switch (target.type) {
+        case `email`:
+          this.setState({
+            emailValidity: true
+          });
+          break;
+        case `password`:
+          this.setState({
+            passValidity: true
+          });
+          break;
+      }
+
+    }
+
+
+    _onFormSubmit(evt) {
+      evt.preventDefault();
+      this._checkValidity(this._isEmailCorrect(), this._isPasswordTyped());
+      if (this._isEmailCorrect() && this._isPasswordTyped()) {
+        this.props.loginUser();
+      }
+    }
+
+
+    render() {
+      return (
+        <Component
+          {...this.props}
+          onSubmit = {this._onFormSubmit}
+          errorMessage = {this.state.errorMessage}
+          onChange = {this._onFormChange}
+          onFocus = {this._onInputFocus}
+          emailValidity = {this.state.emailValidity}
+          passValidity = {this.state.passValidity}
+        />
+      );
+    }
+  }
+
+  WithFormValidation.propTypes = {
+    loginUser: PropTypes.func.isRequired,
+  };
+
+  const mapDispatchToProps = (dispatch) => ({
+    loginUser() {
+      dispatch(ActionCreator.setUserAuth());
+    }
+  });
+
+  return connect(null, mapDispatchToProps)(WithFormValidation);
+
+
+}
