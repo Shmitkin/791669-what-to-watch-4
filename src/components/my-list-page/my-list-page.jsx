0739aa01b2e0a7da3bb34610fe2605c581eebe6d
Shmitkin <mitkinoleg@gmail.com>
2020-07-14 22:12:06 +0300

    Реализуеи загрузку данных на странице избранных фильмов пользователя

--- src/components/my-list-page/my-list-page.jsx
+++ src/components/my-list-page/my-list-page.jsx
@@ -1,28 +1,66 @@
 import React from "react";
+import {connect} from "react-redux";
+import PropTypes from "prop-types";
 
 import PageHeader from "../page-header/page-header.jsx";
 import PageFooter from "../page-footer/page-footer.jsx";
 import MovieCardsList from "../movie-cards-list/movie-cards-list.jsx";
 import UserBlock from "../user-block/user-block.jsx";
 
+import {getUserFavoriteMovies} from "../../reducer/user/selectors.js";
+import {Operation as UserOperation} from "../../reducer/user/user.js";
 
-export default function MyListPage() {
-  return (
-    <div className="user-page">
 
-      <PageHeader extraClass={`user-page__head`}>
-        <h1 className="page-title user-page__title">My list</h1>
-        <UserBlock />
-      </PageHeader>
+class MyListPage extends React.PureComponent {
+  constructor(props) {
+    super(props);
+  }
 
-      <section className="catalog">
-        <h2 className="catalog__title visually-hidden">Catalog</h2>
+  componentDidMount() {
+    const {loadUserFavoriteMovies} = this.props;
+    loadUserFavoriteMovies();
+  }
 
-        <MovieCardsList movies = {[]} />
+  componentDidUpdate(prevProps) {
+    const {loadUserFavoriteMovies, userFavoriteMovies} = this.props;
+    if (prevProps.userFavoriteMovies !== userFavoriteMovies) {
+      loadUserFavoriteMovies();
+    }
+  }
 
-      </section>
-      <PageFooter />
-    </div>
+  render() {
+    const {userFavoriteMovies} = this.props;
 
-  );
+    return (
+      <div className="user-page">
+        <PageHeader extraClass={`user-page__head`}>
+          <h1 className="page-title user-page__title">My list</h1>
+          <UserBlock />
+        </PageHeader>
+        <section className="catalog">
+          <h2 className="catalog__title visually-hidden">Catalog</h2>
+          <MovieCardsList movies = {userFavoriteMovies} />
+        </section>
+        <PageFooter />
+      </div>
+    );
+  }
 }
+
+MyListPage.propTypes = {
+  userFavoriteMovies: PropTypes.array.isRequired,
+  loadUserFavoriteMovies: PropTypes.func.isRequired,
+};
+
+const mapStateToProps = (state) => ({
+  userFavoriteMovies: getUserFavoriteMovies(state)
+});
+
+const mapDispatchToProps = (dispatch) => ({
+  loadUserFavoriteMovies: () => {
+    dispatch(UserOperation.loadFavoriteMovies());
+  }
+});
+
+export {MyListPage};
+export default connect(mapStateToProps, mapDispatchToProps)(MyListPage);
