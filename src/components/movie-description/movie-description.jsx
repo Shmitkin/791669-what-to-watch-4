502d059f7fd8e8a79abfe21070a1e39968298e1b
Shmitkin <mitkinoleg@gmail.com>
2020-07-14 17:52:32 +0300

    Отрисовывает кнопку MyList в на основе пропсов

--- src/components/movie-description/movie-description.jsx
+++ src/components/movie-description/movie-description.jsx
@@ -5,14 +5,16 @@ import MovieButtons from "../movie-buttons/movie-buttons.jsx";
 
 export default function MovieDescription({movie, isMovieDetails}) {
 
+  const {title, genre, release, isFavorite} = movie;
+
   return (
     <div className="movie-card__desc">
-      <h2 className="movie-card__title">{movie.title}</h2>
+      <h2 className="movie-card__title">{title}</h2>
       <p className="movie-card__meta">
-        <span className="movie-card__genre">{movie.genre}</span>
-        <span className="movie-card__year">{movie.release}</span>
+        <span className="movie-card__genre">{genre}</span>
+        <span className="movie-card__year">{release}</span>
       </p>
-      <MovieButtons isMovieDetails = {isMovieDetails}/>
+      <MovieButtons isMovieDetails = {isMovieDetails} isFavorite= {isFavorite}/>
     </div>
   );
 }
@@ -23,6 +25,7 @@ MovieDescription.propTypes = {
     title: PropTypes.string.isRequired,
     genre: PropTypes.string.isRequired,
     release: PropTypes.number.isRequired,
+    isFavorite: PropTypes.bool.isRequired,
   }).isRequired,
   isMovieDetails: PropTypes.bool.isRequired,
 };
--- src/components/my-list-button/my-list-button.jsx
+++ src/components/my-list-button/my-list-button.jsx
@@ -1,14 +1,23 @@
 import React from "react";
+import PropTypes from "prop-types";
 
-export default function MyListButton() {
+export default function MyListButton({isFavorite}) {
 
   return (
     <button className="btn btn--list movie-card__button" type="button">
-      <svg viewBox="0 0 19 20" width="19" height="20">
-        <use xlinkHref="#add"></use>
-      </svg>
+      {isFavorite
+        ? <svg viewBox="0 0 18 14" width="18" height="14">
+          <use xlinkHref="#in-list"></use>
+        </svg>
+        : <svg viewBox="0 0 19 20" width="19" height="20">
+          <use xlinkHref="#add"></use>
+        </svg>
+      }
       <span>My list</span>
     </button>
   );
 }
 
+MyListButton.propTypes = {
+  isFavorite: PropTypes.bool.isRequired,
+};
